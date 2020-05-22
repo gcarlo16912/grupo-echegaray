d281f1aad981864113ec57c52e1a38c527f83d75
giancarlo <gcarlo1691@gmai.com>
2020-05-21 20:05:08 -0500

    avanze del 21-05-2020

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import $ from "jquery";
+import "bootstrap";
+import "./scss/app.scss";
+import "./scss/main.scss";
+
+// Script navbar menu
+$(".dropdown-menu a.dropdown-toggle").on("click", function (e) {
+  if (!$(this).next().hasClass("show")) {
+    $(this).parents(".dropdown-menu").first().find(".show").removeClass("show");
+  }
+  var $subMenu = $(this).next(".dropdown-menu");
+  $subMenu.toggleClass("show");
+
+  $(this)
+    .parents("li.nav-item.dropdown.show")
+    .on("hidden.bs.dropdown", function (e) {
+      $(".dropdown-submenu .show").removeClass("show");
+    });
+
+  return false;
+});
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: `${__dirname}/public`,
+    filename: "app.js",
+  },
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+        ],
+      },
+      {
+        test: /\.pug$/,
+        use: ["html-loader", "pug-html-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/pug/index.pug",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./css/app.css",
+    }),
+  ],
+};
